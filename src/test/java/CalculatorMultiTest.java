33581e07e5b1af6a51bb649fad0b71fbff7353c5
ChrisBruhn <bruhn@eastcape.dk>
2024-06-27 20:40:15 +0200

    removed @Test

--- src/test/java/CalculatorMultiTest.java
+++ src/test/java/CalculatorMultiTest.java
@@ -8,8 +8,15 @@ class CalculatorMultiTest {
     @Test
     public void multiTest(){
         Calculator calculator = new Calculator();
-        int result = calculator.multiply(2,2);
-        Assertions.assertEquals(4,result);
+        int result = calculator.multiply(8,2);
+        Assertions.assertEquals(16,result);
+
+    }
+
+    public void multiZeroTest(){
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(0,2);
+        Assertions.assertEquals(0,result);
 
     }
 }
